e8f115a4189ee53a87e1aa7069baacbe46268246
Vamshi Suram <vamshi.suram25@gmail.com>
2022-10-13 22:30:58 +0530

    basic infinite scroll

---
+++ projects/infinite-scroll/app.js
@@ -0,0 +1,24 @@
+import "./style.css";
+
+const loader = document.querySelector('.loader')
+
+const container = document.querySelector('.container')
+
+const observer = new IntersectionObserver((entries) => {
+  // console.log(entries[0].target.className);
+  if (!entries[0].isIntersecting) return;
+  console.log(entries[0].isIntersecting); // if true trigger new call!
+  // get new entries and append to element
+  let newHtml = ''
+  for (let i = 0; i < 10; i++) {
+    newHtml += `<div class="card">card${i}</div>`
+  }
+  const template = document.createElement('template');
+  template.innerHTML = newHtml;
+  container.insertAdjacentHTML('beforeend', newHtml)
+  // [...template.content.childNodes].forEach(node => {
+  //   container.appendChild(node)
+  // })
+})
+
+observer.observe(loader)
